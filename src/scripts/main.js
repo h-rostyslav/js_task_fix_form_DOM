88aecd78f9b48dfc8b8c94f0f1dcbcc08629b081
Rostyslav` <hrostyslav@ukr.net`>
2026-05-08 13:36:28 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const form = document.querySelector('form');
+const inputs = form.querySelectorAll('input');
+
+inputs.forEach(input => {
+  const label = document.createElement('label');
+  label.classList.add('field-label');
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+
+  const capitalizedName = input.name[0].toUpperCase() + input.name.slice(1);
+
+  input.placeholder = capitalizedName;
+
+  input.parentElement.append(label);
+});
