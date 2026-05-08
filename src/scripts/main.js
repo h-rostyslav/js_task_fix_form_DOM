380642e9fb4e967c303211a22bf969d522ab2162
Rostyslav` <hrostyslav@ukr.net`>
2026-05-08 13:58:24 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,15 +3,16 @@
 const form = document.querySelector('form');
 const inputs = form.querySelectorAll('input');
 
-inputs.forEach(input => {
+inputs.forEach((input) => {
   const label = document.createElement('label');
+
   label.classList.add('field-label');
+  input.id = input.id || input.name;
   label.htmlFor = input.id;
-  label.textContent = input.name;
 
   const capitalizedName = input.name[0].toUpperCase() + input.name.slice(1);
-
+  
+  label.textContent = capitalizedName;
   input.placeholder = capitalizedName;
-
   input.parentElement.append(label);
 });
